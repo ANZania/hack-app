b006f42f96a0439ef89aeed011b25eef8230ee06
Anastasiia <zianastasia@yandex.ru>
2021-05-07 17:55:48 +0300

    Add redux & navigation template

--- App.js
+++ App.js
@@ -1,21 +1,35 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState} from 'react';
+import {Provider} from "react-redux";
+import {store} from "./src/store";
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import AppLoading from "expo-app-loading";
+
+import { StartScreen } from './src/screens/StartScreen'
 
 export default function App() {
+  const [isLoaded, setIsLoaded] = useState(false)
+
+  if (!isLoaded) {
+    return (
+        <AppLoading
+            startAsync={}
+            onFinish={() => setIsLoaded(true)}
+            onError={err => console.log(err)}
+        />
+    )
+  }
+
+  const Stack = createStackNavigator();
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+      <Provider store={store}>
+        <NavigationContainer>
+          <Stack.Navigator>
+            <Stack.Screen name="Start" component={StartScreen} options={{headerShown: false}}/>
+          </Stack.Navigator>
+        </NavigationContainer>
+      </Provider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
