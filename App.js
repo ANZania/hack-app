67b0a10e8159867d4fdee5eea2b2121f668b0214
Iwan <iwan.maksackov@yandex.ru>
2021-05-07 18:33:53 +0300

    fix start bug

--- App.js
+++ App.js
@@ -1,20 +1,15 @@
-import { StatusBar } from 'expo-status-bar';
 import React, {useState} from 'react';
-import {Provider} from "react-redux";
-import {store} from "./src/store";
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import AppLoading from "expo-app-loading";
-
 import { StartScreen } from './src/screens/StartScreen'
 
 export default function App() {
   const [isLoaded, setIsLoaded] = useState(false)
 
-  if (!isLoaded) {
+  if (isLoaded) {
     return (
         <AppLoading
-            startAsync={}
             onFinish={() => setIsLoaded(true)}
             onError={err => console.log(err)}
         />
@@ -24,12 +19,10 @@ export default function App() {
   const Stack = createStackNavigator();
 
   return (
-      <Provider store={store}>
-        <NavigationContainer>
-          <Stack.Navigator>
-            <Stack.Screen name="Start" component={StartScreen} options={{headerShown: false}}/>
-          </Stack.Navigator>
-        </NavigationContainer>
-      </Provider>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Start" component={StartScreen} options={{headerShown: false}}/>
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
--- src/screens/StartScreen.js
+++ src/screens/StartScreen.js
@@ -1,6 +1,11 @@
 import React from 'react'
 import {View} from 'react-native'
 
-export const StartScreen = ({navigation}) => (
-        <View />
-);
+export const StartScreen = ({navigation}) => {
+    return (
+        <View style={{flex: 1, backgroundColor: 'black'}}>
+
+        </View>
+    )
+
+};
