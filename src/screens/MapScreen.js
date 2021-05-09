1c9595df24ef7180ad586c09c3d49e4ec9deab13
Iwan <iwan.maksackov@yandex.ru>
2021-05-09 14:30:03 +0300

    final final

--- src/screens/MapScreen.js
+++ src/screens/MapScreen.js
@@ -18,20 +18,6 @@ import MapView, {
 } from 'react-native-maps';
 
 export const MapScreen = ({navigation}) => {
-  const [location, setLocation] = useState(null);
-  const [errorMsg, setErrorMsg] = useState(null);
-  useEffect(() => {
-    (async () => {
-      let { status } = await Location.requestForegroundPermissionsAsync();
-      if (status !== 'granted') {
-        setErrorMsg('Permission to access location was denied');
-        return;
-      }
-
-      let location = await Location.getCurrentPositionAsync({});
-      setLocation(location);
-    })();
-  }, []);
 
   return (
     <View style={styles.container}>
